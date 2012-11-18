dd3b000bfc2841e57b9cbb69a24ac37dde9664d9
Evgeny Kotelnikov <evgeny.kotelnikov@gmail.com>
2012-11-18 15:15:58 +0400

    Use nice unicode arrows wherever possible

--- src/main/scala/PorterStemmer.scala
+++ src/main/scala/PorterStemmer.scala
@@ -12,86 +12,86 @@ object PorterStemmer {
   def stem(word: String): String = {
     // Deal with plurals and past participles
     var stem = new Word(word).applyReplaces(
-      "sses" -> "ss",
-      "ies" -> "i",
-      "ss" -> "ss",
-      "s" -> "")
+      "sses" → "ss",
+      "ies" → "i",
+      "ss" → "ss",
+      "s" → "")
 
     if ((stem matchedBy ((~v~) + "ed")) ||
         (stem matchedBy ((~v~) + "ing"))) {
 
-      stem = stem.applyReplaces(~v~)("ed" -> "", "ing" -> "")
+      stem = stem.applyReplaces(~v~)("ed" → "", "ing" → "")
 
       stem = stem.applyReplaces(
-        "at" -> "ate",
-        "bl" -> "ble",
-        "iz" -> "ize",
-        (~d and not(~L or ~S or ~Z)) -> singleLetter,
-        (m == 1 and ~o) -> "e")
+        "at" → "ate",
+        "bl" → "ble",
+        "iz" → "ize",
+        (~d and not(~L or ~S or ~Z)) → singleLetter,
+        (m == 1 and ~o) → "e")
     } else {
-      stem = stem.applyReplaces((m > 0) + "eed" -> "ee")
+      stem = stem.applyReplaces((m > 0) + "eed" → "ee")
     }
 
-    stem = stem.applyReplaces((~v~) + "y" -> "i")
+    stem = stem.applyReplaces((~v~) + "y" → "i")
 
     // Remove suffixes
     stem = stem.applyReplaces(m > 0)(
-      "ational" -> "ate",
-      "tional" -> "tion",
-      "enci" -> "ence",
-      "anci" -> "ance",
-      "izer" -> "ize",
-      "abli" -> "able",
-      "alli" -> "al",
-      "entli" -> "ent",
-      "eli" -> "e",
-      "ousli" -> "ous",
-      "ization" -> "ize",
-      "ation" -> "ate",
-      "ator" -> "ate",
-      "alism" -> "al",
-      "iveness" -> "ive",
-      "fulness" -> "ful",
-      "ousness" -> "ous",
-      "aliti" -> "al",
-      "iviti" -> "ive",
-      "biliti" -> "ble")
+      "ational" → "ate",
+      "tional" → "tion",
+      "enci" → "ence",
+      "anci" → "ance",
+      "izer" → "ize",
+      "abli" → "able",
+      "alli" → "al",
+      "entli" → "ent",
+      "eli" → "e",
+      "ousli" → "ous",
+      "ization" → "ize",
+      "ation" → "ate",
+      "ator" → "ate",
+      "alism" → "al",
+      "iveness" → "ive",
+      "fulness" → "ful",
+      "ousness" → "ous",
+      "aliti" → "al",
+      "iviti" → "ive",
+      "biliti" → "ble")
 
     stem = stem.applyReplaces(m > 0)(
-      "icate" -> "ic",
-      "ative" -> "",
-      "alize" -> "al",
-      "iciti" -> "ic",
-      "ical" -> "ic",
-      "ful" -> "",
-      "ness" -> "")
+      "icate" → "ic",
+      "ative" → "",
+      "alize" → "al",
+      "iciti" → "ic",
+      "ical" → "ic",
+      "ful" → "",
+      "ness" → "")
 
     stem = stem.applyReplaces(m > 1)(
-      "al" -> "",
-      "ance" -> "",
-      "ence" -> "",
-      "er" -> "",
-      "ic" -> "",
-      "able" -> "",
-      "ible" -> "",
-      "ant" -> "",
-      "ement" -> "",
-      "ment" -> "",
-      "ent" -> "",
-      (~S or ~T) + "ion" -> "",
-      "ou" -> "",
-      "ism" -> "",
-      "ate" -> "",
-      "iti" -> "",
-      "ous" -> "",
-      "ive" -> "",
-      "ize" -> "")
+      "al" → "",
+      "ance" → "",
+      "ence" → "",
+      "er" → "",
+      "ic" → "",
+      "able" → "",
+      "ible" → "",
+      "ant" → "",
+      "ement" → "",
+      "ment" → "",
+      "ent" → "",
+      (~S or ~T) + "ion" → "",
+      "ou" → "",
+      "ism" → "",
+      "ate" → "",
+      "iti" → "",
+      "ous" → "",
+      "ive" → "",
+      "ize" → "")
 
     // Tide up a little bit
-    stem = stem applyReplaces((m > 1) + "e" -> "",
-                              ((m == 1) and not(~o)) + "e" -> "")
+    stem = stem applyReplaces((m > 1) + "e" → "",
+                              ((m == 1) and not(~o)) + "e" → "")
 
-    stem = stem applyReplaces ((m > 1 and ~d and ~L) -> singleLetter)
+    stem = stem applyReplaces ((m > 1 and ~d and ~L) → singleLetter)
 
     stem.toString
   }
@@ -109,23 +109,23 @@ object PorterStemmer {
    * Condition, that is checked against the beginning of the word
    * @param predicate Predicate to be applied to the word
    */
-  private case class Condition(predicate: Word => Boolean) {
+  private case class Condition(predicate: Word ⇒ Boolean) {
     def + = new Pattern(this, _: String)
 
     def unary_~ = this // just syntactic sugar
 
     def ~ = this
     
-    def and(condition: Condition) = Condition((word) => predicate(word) && condition.predicate(word))
+    def and(condition: Condition) = Condition((word) ⇒ predicate(word) && condition.predicate(word))
 
-    def or(condition: Condition) = Condition((word) => predicate(word) || condition.predicate(word))
+    def or(condition: Condition) = Condition((word) ⇒ predicate(word) || condition.predicate(word))
   }
 
-  private def not: Condition => Condition = {
-    case Condition(predicate) => Condition(!predicate(_))
+  private def not: Condition ⇒ Condition = {
+    case Condition(predicate) ⇒ Condition(!predicate(_))
   }
 
-  private val emptyCondition = Condition(_ => true)
+  private val emptyCondition = Condition(_ ⇒ true)
 
   private object m {
     def >(measure: Int) = Condition(_.measure > measure)
@@ -148,7 +148,7 @@ object PorterStemmer {
    * Builder of the stem
    * @param build Function to be called to build a stem
    */
-  private case class StemBuilder(build: Word => Word)
+  private case class StemBuilder(build: Word ⇒ Word)
 
   private def suffixStemBuilder(suffix: String) = StemBuilder(_ + suffix)
 
@@ -167,9 +167,9 @@ object PorterStemmer {
 
     def hasConsonantAt(position: Int): Boolean =
       (word.indices contains position) && (word(position) match {
-        case 'a' | 'e' | 'i' | 'o' | 'u' => false
-        case 'y' if hasConsonantAt(position + 1) => false
-        case _ => true
+        case 'a' | 'e' | 'i' | 'o' | 'u' ⇒ false
+        case 'y' if hasConsonantAt(position + 1) ⇒ false
+        case _ ⇒ true
       })
 
     def hasVowelAt = !hasConsonantAt(_: Int)
@@ -192,22 +192,22 @@ object PorterStemmer {
      * Measure of the word -- the number of VCs
      * @return integer
      */
-    def measure = word.indices.filter(pos => hasVowelAt(pos) && hasConsonantAt(pos + 1)).length
+    def measure = word.indices.filter(pos ⇒ hasVowelAt(pos) && hasConsonantAt(pos + 1)).length
 
-    def matchedBy: Pattern => Boolean = {
-      case Pattern(condition, suffix) =>
+    def matchedBy: Pattern ⇒ Boolean = {
+      case Pattern(condition, suffix) ⇒
         endsWith(suffix) && (trimSuffix(suffix.length) satisfies condition)
     }
 
     def applyReplaces(replaces: (Pattern, StemBuilder)*): Word = {
-      for ((pattern, stemBuilder) <- replaces if matchedBy(pattern))
+      for ((pattern, stemBuilder) ← replaces if matchedBy(pattern))
         return stemBuilder build trimSuffix(pattern.suffix.length)
       this
     }
 
     def applyReplaces(commonCondition: Condition)(replaces: (Pattern, StemBuilder)*): Word =
       applyReplaces(replaces map {
-        case (Pattern(condition, suffix), stemBuilder) =>
+        case (Pattern(condition, suffix), stemBuilder) ⇒
           (Pattern(commonCondition and condition, suffix), stemBuilder)
       }: _*)
 
@@ -216,7 +216,7 @@ object PorterStemmer {
 
   private implicit def pimpMyRule[P <% Pattern, SB <% StemBuilder]
                                  (rule: (P, SB)): (Pattern, StemBuilder) = (rule._1, rule._2)
-  private implicit def emptyConditionPattern: String => Pattern = Pattern(emptyCondition, _)
-  private implicit def emptySuffixPattern: Condition => Pattern = Pattern(_, "")
-  private implicit def suffixedStemBuilder: String => StemBuilder = suffixStemBuilder
+  private implicit def emptyConditionPattern: String ⇒ Pattern = Pattern(emptyCondition, _)
+  private implicit def emptySuffixPattern: Condition ⇒ Pattern = Pattern(_, "")
+  private implicit def suffixedStemBuilder: String ⇒ StemBuilder = suffixStemBuilder
 }
